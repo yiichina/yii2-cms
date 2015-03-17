96963387f85b6614053571607c968868774de744
╃巡洋艦㊣ <admin@yiichina.com>
2015-03-17 20:44:15 +0800

    add title

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-frontend',
+    'name' => 'Yii CMS',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
