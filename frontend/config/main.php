0e0c95d8c0bbc4c39c887dee3885a93a46ada9ba
╃巡洋艦㊣ <admin@yiichina.com>
2015-05-11 20:28:32 +0800

    add user modules

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,6 +12,11 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'modules' => [
+        'user' => [
+            'class' => 'app\modules\user\Module',
+        ],
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
