d33a78190de5a3c59d5c741d97175c3cc42d0b47
cuileon <cuiliang@live.cn>
2017-07-21 14:25:54 +0800

    add sql

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -12,6 +12,7 @@ class m130524_201442_init extends Migration
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
 
+        // 用户信息表
         $this->createTable('{{%user}}', [
             'id' => $this->primaryKey(),
             'username' => $this->string()->notNull()->unique(),
@@ -19,7 +20,36 @@ class m130524_201442_init extends Migration
             'password_hash' => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email' => $this->string()->notNull()->unique(),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
 
+        // 栏目表
+        $this->createTable('{{%node}}', [
+            'id' => $this->primaryKey(),
+            'parent_id' => $this->string()->notNull()->unique(),
+            'name' => $this->smallInteger()->notNull()->defaultValue(10),
+            'description' => $this->smallInteger()->notNull()->defaultValue(10),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        // 模板表
+        $this->createTable('{{%template}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->smallInteger()->notNull()->defaultValue(10),
+            'description' => $this->smallInteger()->notNull()->defaultValue(10),
+            'status' => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+
+        // 文档信息表
+        $this->createTable('{{%post}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->string()->notNull()->unique(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
