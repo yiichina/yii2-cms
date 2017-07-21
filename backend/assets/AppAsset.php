ef1a54ec83234942e8f520a3f5c519a4ccf54c3e
cuileon <cuiliang@live.cn>
2017-07-22 06:41:46 +0800

    修改composer

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -19,5 +19,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yiichina\adminlte\AdminLTEAsset',
     ];
 }
